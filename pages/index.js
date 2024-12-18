a3192ec65e9c1aa92aecf21a2ae1caeae824a4e6
Wellington Gomes <92111361+wellingtoncw@users.noreply.github.com>
2024-12-18 14:49:05 +0000

    texto atualizado lorem ipsum

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Caracas meu primeiro projeto</h1>
+    return <h1>Lorem ipsum mauris primis morbi vulputate odio mi lacus proin pulvinar, turpis amet tortor himenaeos aliquam ultrices nullam porttitor. nostra aptent odio proin tellus mi adipiscing dictum, himenaeos augue porttitor erat suscipit sit tortor venenatis, non in sit turpis ullamcorper lobortis. feugiat hendrerit proin at conubia fusce a nullam habitant, aenean vitae cubilia egestas nostra ultrices rutrum, placerat dictum sit consectetur eget eu diam. pretium in dolor aliquam pulvinar praesent sed fusce platea proin, consequat malesuada sapien aliquam molestie sollicitudin mi neque, sodales laoreet donec etiam ligula ac conubia a.</h1>
 }
 
 export default Home;
