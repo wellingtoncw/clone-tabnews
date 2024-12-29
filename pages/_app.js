b1ae02197a1925d92dcb866c185619ec88960930
Wellington Gomes <92111361+wellingtoncw@users.noreply.github.com>
2024-12-29 00:14:43 +0000

    Implementando Analytics Vercel

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import { Analytics } from "@vercel/analytics/next";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
+
+export default MyApp;
